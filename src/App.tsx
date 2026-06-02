89d47cb593b165a39583624e52a5d3141417d609
selmanpak <selmanpak@gmail.com>
2026-06-02 22:16:54 +0300

    Fix scroll behavior type for build

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ export default function App() {
   const location = useLocation();
 
   useLayoutEffect(() => {
-    window.scrollTo({ top: 0, left: 0, behavior: "instant" });
+    window.scrollTo({ top: 0, left: 0, behavior: "auto" });
   }, [location.pathname]);
 
   return (
